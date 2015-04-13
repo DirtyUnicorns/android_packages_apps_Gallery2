5102578cd7e8656c3fb04898884208dcf348ddd2
Alex Cruz <mazdarider23@gmail.com>
2015-04-13 20:30:10 +0000

    Revert "Gallery2 : Added support for Oma drm (CD and SD)"
    This reverts commit 6144de8cc35fdd114802545c9922a1a234af8397.

--- src/com/android/gallery3d/app/AlbumPage.java
+++ src/com/android/gallery3d/app/AlbumPage.java
@@ -21,7 +21,7 @@ import android.content.Context;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
-import android.drm.DrmManagerClientWrapper;
+import android.drm.DrmManagerClient;
 import android.drm.DrmRights;
 import android.drm.DrmStore.Action;
 import android.drm.DrmStore.DrmDeliveryType;
@@ -367,7 +367,7 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
 
            Log.d(TAG, "pickPhoto:path = " + path);
            if (path != null && (path.endsWith(".dcf") || path.endsWith(".dm"))) {
-                DrmManagerClientWrapper drmClient = new DrmManagerClientWrapper(context);
+                DrmManagerClient drmClient = new DrmManagerClient(context);
                 path = path.replace("/storage/emulated/0", "/storage/emulated/legacy");
                 int status = -1;
                 Log.d(TAG, "pickPhoto:item type = " + Integer.toString(item.getMediaType()));
@@ -381,6 +381,10 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
                         + Integer.toString(status));
 
                 ContentValues values = drmClient.getMetadata(path);
+
+                // This hack is added to work FL. It will remove after the sdcard permission issue solved
+                status = RightsStatus.RIGHTS_VALID;
+
                 if (RightsStatus.RIGHTS_VALID!= status) {
                     String address = values.getAsString("Rights-Issuer");
                     Log.d(TAG, "pickPhoto:address = " + address);
@@ -450,7 +454,7 @@ public class AlbumPage extends ActivityState implements GalleryActionBar.Cluster
                 path = ((LocalMediaItem)item).filePath;
             }
             if (path != null && (path.endsWith(".dcf") || path.endsWith(".dm"))) {
-                DrmManagerClientWrapper drmClient = new DrmManagerClientWrapper((Context) mActivity);
+                DrmManagerClient drmClient = new DrmManagerClient((Context) mActivity);
                 path = path.replace("/storage/emulated/0", "/storage/emulated/legacy");
                 ContentValues values = drmClient.getMetadata(path);
                 int drmType = values.getAsInteger("DRM-TYPE");
--- src/com/android/gallery3d/app/GalleryActivity.java
+++ src/com/android/gallery3d/app/GalleryActivity.java
@@ -24,7 +24,6 @@ import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.database.Cursor;
 import android.drm.DrmManagerClient;
-import android.drm.DrmManagerClientWrapper;
 import android.drm.DrmStore.Action;
 import android.drm.DrmStore.DrmDeliveryType;
 import android.drm.DrmStore.RightsStatus;
@@ -253,8 +252,12 @@ public final class GalleryActivity extends AbstractGalleryActivity implements On
 
                 Log.d(TAG,"imagePath=" + imagePath);
                 if (intent.getBooleanExtra("WidgetClick", false) == true) {
-                    DrmManagerClientWrapper drmClient = new DrmManagerClientWrapper(this);
+                    DrmManagerClient drmClient = new DrmManagerClient(this);
+
+                    // This hack is added to work FL. It will remove after the sdcard permission issue solved
                     int status = drmClient.checkRightsStatus(imagePath, Action.DISPLAY);
+                    status = RightsStatus.RIGHTS_VALID;
+
                     if (RightsStatus.RIGHTS_VALID != status) {
                         ContentValues values = drmClient.getMetadata(imagePath);
                         String address = values.getAsString("Rights-Issuer");
@@ -268,7 +271,7 @@ public final class GalleryActivity extends AbstractGalleryActivity implements On
 
                 if (imagePath != null
                         && (imagePath.endsWith(".dcf") || imagePath.endsWith(".dm"))) {
-                    DrmManagerClientWrapper drmClient = new DrmManagerClientWrapper(this);
+                    DrmManagerClient drmClient = new DrmManagerClient(this);
                     imagePath = imagePath.replace("/storage/emulated/0", "/storage/emulated/legacy");
                     ContentValues values = drmClient.getMetadata(imagePath);
                     int drmType = values.getAsInteger("DRM-TYPE");
