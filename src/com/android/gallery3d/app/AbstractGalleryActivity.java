6a32f7171ad390e1d468d28019800665ed612993
maxwen <max.weninger@gmail.com>
2018-03-09 20:40:16 -0500

    Prompt for location permission
    Extracted the code to prompt for location permission from two commits
    
    https://github.com/omnirom/android_packages_apps_Gallery2/commit/9b62fbdd9d4e859ee74cba60ef9d550e615507ec
    
    https://github.com/omnirom/android_packages_apps_Gallery2/commit/16ee1db5742df82884a6f08dd5ec867656cfb1a1
    
    Didn't need the other stuff that Max was tryign to do :-)
    
    Change-Id: I8e19d8813af0e6baab466654a1130b4c981c4d50

--- src/com/android/gallery3d/app/AbstractGalleryActivity.java
+++ src/com/android/gallery3d/app/AbstractGalleryActivity.java
@@ -16,6 +16,7 @@
 
 package com.android.gallery3d.app;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -27,6 +28,7 @@ import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.content.ServiceConnection;
 import android.content.res.Configuration;
 import android.net.Uri;
@@ -50,9 +52,13 @@ import com.android.gallery3d.util.ThreadPool;
 import com.android.photos.data.GalleryBitmapPool;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class AbstractGalleryActivity extends Activity implements GalleryContext {
     private static final String TAG = "AbstractGalleryActivity";
+
+    private static final int PERMISSION_REQUEST_LOCATION = 2;
+
     private GLRootView mGLRootView;
     private StateManager mStateManager;
     private GalleryActionBar mActionBar;
@@ -69,6 +75,8 @@ public class AbstractGalleryActivity extends Activity implements GalleryContext 
         }
     };
     private IntentFilter mMountFilter = new IntentFilter(Intent.ACTION_MEDIA_MOUNTED);
+    private Runnable mRunWithPermission;
+    private Runnable mRunWithoutPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -364,5 +372,61 @@ public class AbstractGalleryActivity extends Activity implements GalleryContext 
         } catch (FileNotFoundException fnfe) {
             Log.e(TAG, "Error printing an image", fnfe);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+            int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_LOCATION: {
+                if (checkPermissionGrantResults(grantResults)) {
+                    if (mRunWithPermission != null) {
+                        mRunWithPermission.run();
+                    }
+                } else {
+                    if (mRunWithoutPermission != null) {
+                        mRunWithoutPermission.run();
+                    }
+                }
+            }
+        }
+    }
+
+    public void doRunWithLocationPermission(Runnable runWithPermission, Runnable runWithoutPermission) {
+        mRunWithPermission = runWithPermission;
+        mRunWithoutPermission = runWithoutPermission;
+        String[] permissions = {
+                Manifest.permission.ACCESS_FINE_LOCATION
+        };
+
+        if (!hasLocationPermission()) {
+            requestPermissions(permissions, PERMISSION_REQUEST_LOCATION);
+        }  else {
+            runWithPermission.run();
+        }
+    }
+
+    public boolean hasLocationPermission() {
+        boolean needRequest = false;
+
+        String[] permissions = {
+                Manifest.permission.ACCESS_FINE_LOCATION
+        };
+        for (String permission : permissions) {
+            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                needRequest = true;
+                break;
+            }
+        }
+        return !needRequest;
+    }
+
+    private boolean checkPermissionGrantResults(int[] grantResults) {
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 }
